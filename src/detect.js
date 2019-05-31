df0b0c7f50f6918a718d02ff535dd913341a59ed
belinhacbr <belinhacbr@gmail.com>
2019-05-31 01:30:27 +0100

    cleanup

--- src/detect.js
+++ src/detect.js
@@ -4,36 +4,46 @@ const fileUpload = document.getElementById('fileUpload');
 const base_image = new Image();
 base_image.src = 'assets/smiley.png';
 
-function detectFace(objSrc, objId, container, camera){
-    console.log('Reading ' + objId);
-    prepareCanvas(objSrc);
+var _objSrc = document.getElementById('img');
+var _objId = '#img';
+
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+function detectFace(camera){
     var tracker = new tracking.ObjectTracker('face');
-    tracker.setStepSize(1.5);
-    tracker.setInitialScale(4);
-    tracker.setEdgesDensity(0.1);
-    tracking.track(objId, tracker, {camera : camera});
-    tracker.on('track', function(event) {
-      console.log('detecting..')
-      prepareCanvas(objSrc);
-      event.data.forEach(function(rect) {
-        drawOnCanvas(rect, objSrc);
-      });
+    console.log('Reading ' + _objId);
+    prepareCanvas();
+    prepareTracker(tracker, camera);
+    tracker.on('track', track);
+};
+
+function track(event){
+    prepareCanvas();
+    event.data.forEach(function(rect) {
+        drawOnCanvas(rect);
     });
 };
 
 
-function prepareCanvas(objSrc){
-  var canvas = document.getElementById('canvas');
-  var context = canvas.getContext('2d');
+function prepareTracker(tracker, camera){
+  tracker.setStepSize(1.5);
+  tracker.setInitialScale(4);
+  tracker.setEdgesDensity(0.1);
+  tracking.track(_objId, tracker, {camera: camera});
+}
+
+
+function prepareCanvas(){
+  context.save();
   context.clearRect(0, 0, canvas.width, canvas.height);
-  canvas.width = objSrc.width;
-  canvas.height = objSrc.height;
+  canvas.width = _objSrc.width;
+  canvas.height = _objSrc.height;
+  context.restore();
 };
 
 
-function drawOnCanvas(rect, objSrc) {
-  var canvas = document.getElementById('canvas');
-  var context = canvas.getContext('2d');
+function drawOnCanvas(rect) {
   context.drawImage(base_image, rect.x, rect.y, rect.width, rect.height);
   console.log('faces!');
 };
@@ -41,14 +51,12 @@ function drawOnCanvas(rect, objSrc) {
 
 function readImage() {
     var img = document.getElementById('img');
-    slider.checked = true;
-    loadSelectedCanvas();
     if ( this.files && this.files[0] ) {
         var FR = new FileReader();
         FR.onload = function(e) {
            img.src = e.target.result;
            img.onload = function(){
-            detectFace(img, '#img', '.img-container');
+            detectFace(false);
           };
         };
         FR.readAsDataURL( this.files[0] );
@@ -57,8 +65,7 @@ function readImage() {
 
 
 function reloadDetection(){
-  var img = document.getElementById('img');
-  detectFace(img, '#img', '.img-container');
+  detectFace(false);
 };
 
 
@@ -86,30 +93,36 @@ function readVideo(stream){
   window.stream = stream;
   var video = document.getElementById("video")
   video.srcObject = stream;
-  detectFace(video, '#video', '.cam-container', true);
+  detectFace(true);
 };
 
 
-function stopVideo(err){
+function stopVideo(){
   var video = document.getElementById("video")
   if (video && video.srcObject) {
     video.srcObject.getTracks().forEach(function(track){track.stop();});
+  }
+  if (window.stream) {
+    window.stream.getTracks().forEach(function(track){track.stop();});
   }
 };
 
 
 function loadSelectedCanvas(){
   console.log(slider.checked ? 'selected img' : 'selected cam')
-  var frame = document.getElementById('frame');
 
   if (slider.checked == true) {
     stopVideo();
-    const imgHTML = "<div class='img-container'><img id='img' src='assets/band.jpg' onclick='reloadDetection()'/></div>"
-    frame.innerHTML = imgHTML;
-    readImage();
+    document.getElementById('video').parentElement.style.display = 'none';
+    document.getElementById('img').parentElement.style.display = 'block';
+    _objSrc = document.getElementById('img')
+    _objId = '#img';
+    reloadDetection();
   } else {
-    const videoHTML = "<div class='cam-container'><video id='video' width='800' height='400' preload autoplay loop muted></video></div>"
-    frame.innerHTML = videoHTML;
+    document.getElementById('img').parentElement.style.display = 'none';
+    document.getElementById('video').parentElement.style.display = 'block';
+    _objSrc = document.getElementById('video')
+    _objId = '#video';
     openVideo();
   }
 };
